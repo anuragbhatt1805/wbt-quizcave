6d7060290f319dba9d0aaba906194f3c984a7ef3
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-05-25 19:57:35 +0530

    Generated Models for Application

---
+++ src/model/Contest.model.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const QuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    questionImage: {
+        type: String
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['mcq', 'short', 'long', 'numerical', 'multiple']
+    },
+    mcqOptions: {
+        type: [String]
+    },
+    answer: {
+        type: String
+    },
+    multipleQuestion: {
+        type: [String]
+    },
+    multipleAnswer: {
+        type: [String]
+    },
+    marks: {
+        type: Number,
+        required: true
+    }
+});
+
+const ContestSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    duration:{
+        type: Number,
+        required: true
+    },
+    rules: {
+        type: String,
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    questions: {
+        type: [QuestionSchema]
+    },
+    active: {
+        type: Boolean,
+        default: false
+    },
+    registered: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'User'
+    },
+    participants: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'User'
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+export const Contest = mongoose.model('Contest', ContestSchema);
