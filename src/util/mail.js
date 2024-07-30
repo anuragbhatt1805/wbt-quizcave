d3631348ab7219fc7e4a3f34a79e503bcaca39e8
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-07-30 12:07:21 +0530

    chore: Update SMTP server information for email sending

--- src/util/mail.js
+++ src/util/mail.js
@@ -3,25 +3,22 @@ import nodemailer from 'nodemailer';
 
 // Create a transporter using SMTP server information from process.env
 const transporter = nodemailer.createTransport({
-    host: "smtpout.secureserver.net",
-    port: 465,
-    secure: true, // Set to true if using SSL/TLS
-	secureConnection: false,
-	requireTLS: true,
-	debug: true,
+    host: 'smtp-mail.outlook.com',
+    port: '587',
+    secure: false, // Set to true if using SSL/TLS
     auth: {
-        user: "engineering.tests@whiteboardtec.com",
-        pass: "Vision**%%21",
+        user: 'no-reply.whiteboard@outlook.com',
+        pass: 'Qwerty!23456',
     },
     tls: {
-       ciphers: 'SSLv3'
+        ciphers:'SSLv3'
     }
 });
 
 // Define the email options
 export function sendMail(recipient, subject, text, html) {
     const mailOptions = {
-        from: 'engineering.tests@whiteboardtec.com',
+        from: 'no-reply.whiteboard@outlook.com',
         to: recipient,
         subject: subject,
         text: text,
